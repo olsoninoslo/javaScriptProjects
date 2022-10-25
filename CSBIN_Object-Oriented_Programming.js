fc6c7582cabc10a57a46b3b5a94d39b2942ce596
olsoninoslo <107144841+olsoninoslo@users.noreply.github.com>
2022-10-25 10:51:23 -0600

    Update CSBIN_Object-Oriented_Programming.js

--- CSBIN_Object-Oriented_Programming.js
+++ CSBIN_Object-Oriented_Programming.js
@@ -264,3 +264,43 @@ robotFido = Object.assign(robotFido, robotMixin);/* Put code here to give Fido r
 // /********* Uncomment to test your work! *********/
 robotFido.speak() // -> Logs "I am made of metal"
 
+
+
+// Extra: Implementing Gettters and Setters
+
+const menu = {
+  _meal: "_",
+  _price: 0,
+
+  // setter
+  set mealToCheck(meal) {
+    if (typeof meal == "string") {
+      this._meal = meal;
+    } else {
+      console.log("Type Error: Meals are strings");
+    }
+  },
+  set priceToCheck(price) {
+    if (typeof price == "number") {
+      this._price = price;
+    } else {
+      console.log("Type Error: Prices are numbers");
+    }
+  },
+
+  //getter
+  get todaysSpecials() {
+    if (typeof this._meal == "string" && typeof this._price == "number") {
+      return `Todays Special is ${this._meal} for $${this._price}!`;
+    } else {
+      return "Meal or price was not set correctly!";
+    }
+  },
+};
+
+menu.mealToCheck = "Pasta Ragu";
+menu.priceToCheck = 21.99;
+
+// console.log(menu._price)
+
+// console.log(menu.todaysSpecials);
